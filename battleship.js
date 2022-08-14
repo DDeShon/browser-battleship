0b0fe4d4ed7481977ceff76577680c73290d46cf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-08-14 08:58:14 -0500

    added js logic

--- battleship.js
+++ battleship.js
@@ -7,3 +7,13 @@ var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
+
+while (!isSunk) {
+  guess = prompt("Ready, aim, fire! (enter a number from 0 - 6");
+
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid number");
+  } else {
+    guesses = guesses + 1;
+  }
+}
