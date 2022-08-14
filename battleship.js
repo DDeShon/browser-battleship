553d6003417060d5a676eefdd5dab167f605c699
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-08-14 09:18:26 -0500

    added js logic

--- battleship.js
+++ battleship.js
@@ -17,8 +17,8 @@ while (!isSunk) {
     guesses = guesses + 1;
 
     if (guess == location1 || guess == location2 || guess == location3) {
-      hits = hits + 1;
       alert("You scored a hit!");
+      hits = hits + 1;
 
       if (hits == 3) {
         isSunk = true;
